228b2502ed7eaae39fb7136950eb04373f8379d8
Ryan Phillips <ryanmphill@gmail.com>
2023-04-27 11:01:07 -0500

    Added event listener and metadata state to <li> in Walkers.js

--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -2,6 +2,19 @@ import { getWalkers } from "./database.js"  // Changed getWalker to getWalkers
 
 const walkers = getWalkers()
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const whatWasClickedOn = clickEvent.target    
+        /* Target picks out element that was clicked on. 'dataset' is a propety of 
+        that element that uses DOMStringMap to store all the data attributes 
+        as an object. */
+        if (whatWasClickedOn.dataset.type === 'walker') {
+            window.alert(`This walker works in ${whatWasClickedOn.dataset.city}`)
+        }
+        
+    }
+)
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
@@ -9,6 +22,7 @@ export const Walkers = () => {
     for (const walker of walkers) {
         walkerHTML += `<li data-id="${walker.id}"
         data-city="${walker.city}"
+        data-type="walker"
         >${walker.name}</li>` //Changed fullName to name
     }
 
