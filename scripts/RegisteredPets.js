8032e8576e7fbf332c660d88ed36b613b5b73724
Ryan Phillips <ryanmphill@gmail.com>
2023-04-27 12:10:22 -0500

    Added event listener to RegisteredPets that accesses foreign key to display walker associated with pet

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,31 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const clickTarget = clickEvent.target
+        const walkerId = clickTarget.dataset.walkerForeignKey //The DOMStringMap converts the additional dashes in walker-foreign-key to snake case within the dataset object
+
+        // Find the whole walker object based on the walkerId from above
+        for (const walker of walkers) {
+            if (walkerId === `${walker.id}`) { //Remember that walkerId (from the dataset) is stored as a string. You can use 'parseInt' to turn into string, or use a template literal.
+                window.alert(`This pet is being walked by ${walker.name}`)
+                
+            }
+        }
+        
+    } 
+)
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
-
+    //The DOMStringMap converts the additional dashes in walker-foreign-key to snake case within the dataset object
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li data-walker-foreign-key="${pet.walkerId}" 
+        >${pet.name}</li>`
     }
 
     petHTML += "</ul>"
