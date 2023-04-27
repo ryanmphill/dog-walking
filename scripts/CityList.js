94cfce44c5c71c1f8a4e0e810f29b58df0595810
Ryan Phillips <ryanmphill@gmail.com>
2023-04-27 13:38:05 -0500

    Added additional state to CityList and added if statement to Registered Pets

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -2,12 +2,24 @@ import { getWalkers } from "./database.js" // Added an 's' to 'getWalkers'
 
 const walkers = getWalkers()  // Added an 's' to 'getWalkers'
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const cityTarget = clickEvent.target
+        if (cityTarget.dataset.type === 'city') {
+            window.alert(`${cityTarget.dataset.walkerName} is servicing this city`)
+        }
+        
+    }
+)
 
 export const CityList = () => {
     let citiesHTML = "<ul>"  // changed to unordered list
 
     for (const walker of walkers) {
-        citiesHTML += `<li>${walker.city}</li>` // Changed currentWalker to walker
+        citiesHTML += `<li data-walker-name="${walker.name}"
+        data-type="city"
+        >${walker.city}</li>` // Changed currentWalker to walker
     }
 
     citiesHTML += "</ul>"
--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -10,13 +10,14 @@ document.addEventListener(
         const walkerId = clickTarget.dataset.walkerForeignKey //The DOMStringMap converts the additional dashes in walker-foreign-key to snake case within the dataset object
 
         // Find the whole walker object based on the walkerId from above
-        for (const walker of walkers) {
-            if (walkerId === `${walker.id}`) { //Remember that walkerId (from the dataset) is stored as a string. You can use 'parseInt' to turn into string, or use a template literal.
-                window.alert(`This pet is being walked by ${walker.name}`)
-                
+        if (clickTarget.dataset.type === 'pet') {
+            for (const walker of walkers) {
+                if (walkerId === `${walker.id}`) { //Remember that walkerId (from the dataset) is stored as a string. You can use 'parseInt' to turn into string, or use a template literal.
+                    window.alert(`This pet is being walked by ${walker.name}`)
+                    
+                }
             }
-        }
-        
+        }  
     } 
 )
 
@@ -24,7 +25,8 @@ export const RegisteredPets = () => {
     let petHTML = "<ul>"
     //The DOMStringMap converts the additional dashes in walker-foreign-key to snake case within the dataset object
     for (const pet of pets) {
-        petHTML += `<li data-walker-foreign-key="${pet.walkerId}" 
+        petHTML += `<li data-walker-foreign-key="${pet.walkerId}"
+        data-type="pet" 
         >${pet.name}</li>`
     }
 
